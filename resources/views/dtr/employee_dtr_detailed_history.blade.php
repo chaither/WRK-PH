325a6c84967277381e115aef3fcb42279c9c08bd
Cyrhil Dwight Lozano <cyrhildwight@gmail.com>
2025-12-03 18:55:14 +0800

    adding filter on dtr and also the adding profile details on employee and not clickable the clock in and out if the employee is on leave

--- resources/views/dtr/employee_dtr_detailed_history.blade.php
+++ resources/views/dtr/employee_dtr_detailed_history.blade.php
@@ -19,7 +19,7 @@
                 <i class="fas fa-filter mr-2"></i> Filter
             </button>
             <a href="{{ route('employees.profile', $employee->id) }}" class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                <i class="fas fa-arrow-left mr-2"></i> Back to Employee List
+                <i class="fas fa-arrow-left mr-2"></i> Back to Profile
             </a>
         </div>
     </header>
