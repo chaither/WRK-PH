2c0ac00753637cf45cb33a932fb4b7718367bbc6
Cyrhil Dwight Lozano <cyrhildwight@gmail.com>
2026-01-08 09:37:18 +0800

    fixing the time helper

--- app/Helpers/TimeHelper.php
+++ app/Helpers/TimeHelper.php
@@ -8,8 +8,8 @@ class TimeHelper
 {
     public static function getTimeOfDay(Carbon $time)
     {
-        // Define the midday boundary (12:00 PM)
-        $midday = Carbon::createFromTime(12, 0, 0, $time->timezone);
+        // Define the midday boundary (12:00 PM) on the same date as the time being checked
+        $midday = $time->copy()->setTime(12, 0, 0);
 
         if ($time->lessThan($midday)) {
             return 'Morning';
